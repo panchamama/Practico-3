9b863be5ac7661b0e8e01f67e8d2dedc70f06834
panchamama <f.apablaza13@gmail.com>
2024-04-02 21:03:14 -0300

    quarto

--- 2. Proc/Scrip 3 Preparacion.R
+++ 2. Proc/Scrip 3 Preparacion.R
@@ -1,3 +1,24 @@
 # Practico 3: Preparacion de la Base de datos #
 
-# Preparacion de la Base de datos#
+# Paso 0: Conceptualizacion general #
+# Titulo: Latinobarometro
+# Autor:
+# Resumen: Es un estudio de opinion pública
+
+# Paso 1: Librerias #
+install.packages("pacman")
+pacman::p_load(dplyr, sjmisc, car, sjlabelled, stargazer, haven)
+
+# Paso 2: Cargar base de datos #
+rm(list=ls())       # borrar todos los objetos en el espacio de trabajo
+options(scipen=999) # valores sin notación científica
+
+## Base de Datos ##
+load(url("https://github.com/Kevin-carrasco/metod1-MCS/raw/main/files/data/external_data/latinobarometro2020.RData"))
+
+
+
+
+
+
+
